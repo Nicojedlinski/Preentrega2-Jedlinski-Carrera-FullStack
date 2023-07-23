2daa38b9043e7393539722027f2f4d19fba2a95c
nico jedlinskki <nicolasjedlinski@outlook.com>
2023-07-23 20:40:17 -0300

    agrego js primeros pasos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-//Comenatrio
+let edad= 23
+edad=30
+
+let nombre= "nicolas"
+
+let usuario= prompt ("Welcome") 
+
+alert ("Bienvenido" + "  " + usuario)
+
+edad=prompt("que edad tienes ?")
+
+alert ("eres mayor de 18 ?")
+
+edad=prompt("si o no ?")
+
+alert ("Gracias Por tu respuesta")
